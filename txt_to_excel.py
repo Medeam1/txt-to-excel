6a2459e3504885eeb5785cc762159c802c6c2dcc
Georgi Dimov <dimov.georgi1998@gmail.com>
2025-01-19 15:05:12 +0200

    changes

--- txt_to_excel.py
+++ txt_to_excel.py
@@ -3,16 +3,13 @@ import pandas as pd
 
 def convert_txt_to_excel(input_file, output_file):
     try:
-        # Четене на текстовия файл
         data = pd.read_csv(input_file, sep='|', header=None)
 
-        # Запис на Excel файла с правилния двигател
         data.to_excel(output_file, index=False, header=False, engine='openpyxl')
         print(f"Данните са успешно записани в {output_file}")
     except Exception as e:
         print(f"Възникна грешка: {e}")
 
-# Пример за пътища
 
 
 input_file_name = input('Име на текстовия файл: ')
